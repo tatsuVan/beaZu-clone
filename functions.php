831eb3fbd7db8f872d9581843e2069f070baaa57
池山達也 <ikeyamatatsuya@ikeyamatatsuyas-MacBook-Pro.local>
2023-11-01 17:48:56 -0700

    fixed details like responsive bug

--- functions.php
+++ functions.php
@@ -223,3 +223,42 @@ function woocommerce_header_add_to_cart_fragment( $fragments ) {
  * WooCommerce
  */
 add_theme_support('woocommerce');
+
+
+/**
+ * Set WooCommerce image dimensions upon theme activation
+ */
+// Remove each style one by one
+add_filter( 'woocommerce_enqueue_styles', 'jk_dequeue_styles' );
+function jk_dequeue_styles( $enqueue_styles ) {
+	unset( $enqueue_styles['woocommerce-general'] );	// Remove the gloss
+	// unset( $enqueue_styles['woocommerce-layout'] );		// Remove the layout
+	// unset( $enqueue_styles['woocommerce-smallscreen'] );	// Remove the smallscreen optimisation
+	return $enqueue_styles;
+}
+
+// remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+// remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+// add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
+// add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
+
+// function my_theme_wrapper_start() {
+//     echo '<section id="main">';
+// }
+
+// function my_theme_wrapper_end() {
+//     echo '</section>';
+// }
+
+
+/**
+ * Enqueue your own stylesheet
+ */
+function wp_enqueue_woocommerce_style(){
+	wp_register_style( 'mytheme-woocommerce', get_template_directory_uri() . '/css/woocommerce/woocommerce.css' );
+	
+	if ( class_exists( 'woocommerce' ) ) {
+		wp_enqueue_style( 'mytheme-woocommerce' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'wp_enqueue_woocommerce_style' );
